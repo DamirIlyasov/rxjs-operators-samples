729ed537848d4093dd7951006fd06ad74e1ada92
DamirIlyasov <damirilyasov1997@gmail.com>
2018-12-06 20:09:37 +0300

    almost all text added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,20 +24,151 @@ import {CustomConsole} from './custom-console';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Operators';
 
-  // this.tap();
-  // this.combineLatest();
-  // this.concat();
-  // this.merge();
-  // this.startWith();
-  // this.withLatestFrom();
-  // this.from();
-  // this.of();
-  // this.catchError();
-  // this.debounceTime();
-  // this.distinctUntilChanged();
-  // this.filter();
+  codeMirrorOptions = {lineNumbers: true, theme: 'idea', mode: 'javascript'};
+
+  tapText = `
+    const firstObs = of(1, 2, 3);
+    firstObs.pipe(
+      tap(v => {
+        console.log('tap: ', v);
+      })
+    ).subscribe(v => {
+      console.log(v);
+    });
+  `;
+
+  combineLatestText = `
+    const firstObs = new Observable(observer => {
+      setTimeout(() => observer.next(1), 4000);
+    });
+    const secondObs = new Observable(observer => {
+      observer.next(1);
+      setTimeout(() => observer.next(2), 2000);
+    });
+
+    const thirdObs = new Observable(observer => {
+      observer.next(1);
+      setTimeout(() => observer.next(2), 2000);
+    });
+
+    const result = combineLatest(firstObs, secondObs, thirdObs);
+    result.subscribe(([first, second, third]) => {
+      console.log(\`First: \`, first);
+      console.log(\`Second: \`, second);
+      console.log(\`Third: \`, third);
+    });
+  `;
+
+  concatText = `
+    const firstObs = of(1, 2, 3);
+    const secondObs = of(4, 5, 6);
+    const result = concat(firstObs, secondObs);
+    result.subscribe(value => {
+      console.log(value);
+    });
+  `;
+
+  mergeText = `
+    const firstObs = of(1, 1, 1);
+    const secondObs = of(2, 2, 2);
+    const merged = merge(firstObs, secondObs);
+    merged.subscribe(value => {
+      console.log(value);
+    });
+  `;
+
+  startWithText = `
+    const firstObs = of(1, 2, 3);
+    const result = firstObs.pipe(startWith(0));
+    result.subscribe(value => {
+      console.log(value);
+    });
+  `;
+
+  withLatestFromText = `
+    const firstObs = interval(5000);
+    const secondObs = interval(1000);
+    const result = firstObs.pipe(
+      withLatestFrom(secondObs),
+      tap(([firstValue, secondValue]) => {
+        console.log('First value: ', firstValue);
+        console.log('Second value: ', secondValue);
+      })
+    );
+    result.subscribe(() => {
+    });
+  `;
+
+  fromText = `
+    // Turn an array, promise, or iterable into an observable
+    // берёт один объект
+    from([{qwe: 'qwe'}]);
+    const firstObs = from([1, 2, 3, 4]);
+    firstObs.subscribe(value => {
+      console.log('Value: ', value);
+    });
+  `;
+
+  ofText = `
+    const object: {
+      field1: string,
+      field2: number
+    } = {
+      field1: 'qwe',
+      field2: 123
+    };
+
+    const obs = of(object, () => {
+      console.log('I\`m a function inside observable');
+    });
+    obs.subscribe(value => {
+      console.log(value);
+    });
+  `;
+
+  catchErrorText = `
+    const errorObservable = new Observable(observer => {
+      observer.error('Error value');
+      // вторая ошибка не будет обработана, т.к. catchError завершает observable
+      observer.error('Error value');
+    });
+
+
+    // errorObservable.subscribe(() => {
+    //   console.log('Everything is fine!');
+    // });
+    // ошибка вывелась в браузер, т.к. мы её не обработали
+
+    // теперь обработаем:
+    errorObservable.pipe(
+      catchError(error => {
+        console.log('Error caught: ', error);
+        return of('Error caught: ', error);
+      })
+    ).subscribe();
+  `;
+
+  debounceTimeText = `
+    const button = document.getElementById('F');
+    // fromEvent(source, eventName) - превращает всё это дело в обсервабл
+    // интервал между нажатиями > 2с
+    const observable = fromEvent(button, 'click').pipe(debounceTime(2000));
+    observable.subscribe(() => console.log('hah, not working :('));
+  `;
+
+  distinctUntilChangedText = `
+    const observable = of(1, 1, 2, 2, 3, 3, 4, 4);
+    observable.pipe(distinctUntilChanged()).subscribe(v => console.log(v));
+  `;
+
+  filterText = `
+    const observable = of(1, 1, 2, 2, 3, 3, 4, 4);
+    observable.pipe(
+      filter(v => v > 2)
+    ).subscribe(v => console.log(v));
+  `;
+
   // this.take();
   // this.takeUntil();
   // this.share(); НЕ ГОТОВО
